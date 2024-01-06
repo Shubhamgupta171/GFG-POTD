f7cece189ce45f5d8040529c8f6f1ef2dbf8636f
Shubham Gupta <78054098+Shubhamgupta171@users.noreply.github.com>
2024-01-06 17:22:26 +0530

    Create 6Jan.cpp

---
+++ 6Jan.cpp
@@ -0,0 +1,22 @@
+//Tech fest and Queues 
+  class Solution {
+public:
+	int sumOfPowers(int a, int b){
+	       int count=0;
+        for(int i=a;i<=b;i++)
+         {
+            int n=i;
+            for(int j=2;j<=sqrt(n);j++)
+            {
+                while(n%j==0)
+                {
+                    count++;
+                    n=n/j;
+               }
+            }
+            if(n>=2)
+            count++;
+         }
+        return count;
+    }
+};
